149d921292047e37cec91456b73f3ddf07777943
davidneverend <simphy@mailbox.org>
2024-11-28 17:44:32 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 clear
+echo "Applying dark mode"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+sleep 2
+
+clear
 echo "Refreshing keyrings..."
 echo
-sudo pacman -Sy
+sudo pacman -Syu
 sudo pacman -S archlinux-keyring
+sleep 2
+
+clear
+echo "Installing git"
+sudo pacman -S git
 sleep 2
 
 clear
@@ -17,20 +27,23 @@ makepkg -si
 sleep 2
 
 clear
-echo "Installing Firefox & Thunderbird & LibreOffice & Spotify & Sublime-Text"
-sudo pacman -S firefox thunderbird libreoffice-fresh spotify-launcher
+echo "Installing apps"
+sudo pacman -S nautilus firefox thunderbird gvfs-smb git libreoffice-fresh spotify-launcher waybar python-requests ttf-font-awesome noto-fonts-emoji gnu-free-fonts 
 yay -S sublime-text-4
 sleep 2
 
-clear
-echo "Applying dark mode"
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+clear 
+echo "Remove apps"
+sudo pacman -R dolphin vim
 sleep 2
 
-clear
-cd
-git clone https://github.com/davidneverend/hyprland.git
-cp -f hyprland/config/hypr/hyprland.conf /home/david/.config/hypr/hyprland.conf 
-cp -f hyprland/config/kitty/kitty.conf /home/david/.config/kitty/kitty.conf
-cp -f hyprland/.bashrc /home/david/.bashrc
 
+#clear
+#cd
+#git clone https://github.com/davidneverend/hyprland.git
+#cp -f hyprland/config/hypr/hyprland.conf /home/david/.config/hypr/hyprland.conf 
+#cp -f hyprland/config/kitty/kitty.conf /home/david/.config/kitty/kitty.conf
+#cp -f hyprland/.bashrc /home/david/.bashrc
+
+# A voir :
+#hyprpaper
